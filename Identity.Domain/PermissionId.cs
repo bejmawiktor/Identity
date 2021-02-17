1dbf0cb356586b666e50d42dfae38448c930c962
BlekU <bejmawiktor@gmail.com>
2021-02-17 21:39:05 +0100

    PermissionId added.

---
+++ Identity.Domain/PermissionId.cs
@@ -0,0 +1,31 @@
+﻿using DDD.Model;
+using System;
+using System.Text.RegularExpressions;
+
+namespace Identity.Domain
+{
+    public class PermissionId : Identifier<string, PermissionId>
+    {
+        public string AlphaNumericPattern => "^[a-zA-Z0-9]*$";
+
+        public PermissionId(string name) : base(name)
+        {
+        }
+
+        protected override void ValidateValue(string value)
+        {
+            if(value.Length == 0)
+            {
+                throw new ArgumentException("Permision id can't be empty.");
+            }
+
+            if(!Regex.IsMatch(value, this.AlphaNumericPattern))
+            {
+                throw new ArgumentException("Permision id must contain only alphanumeric characters without spaces.");
+            }
+        }
+
+        public override string ToString()
+            => this.Value;
+    }
+}
---
+++ Identity.Tests/Unit/Domain/PermissionIdTest.cs
@@ -0,0 +1,45 @@
+﻿using Identity.Domain;
+using NUnit.Framework;
+using System;
+
+namespace Identity.Tests.Unit.Domain
+{
+    [TestFixture]
+    public class PermissionIdTest
+    {
+        [Test]
+        public void TestConstructing_WhenEmptyNameGiven_ThenArgumentExceptionIsThrown()
+        {
+            Assert.Throws(
+              Is.InstanceOf<ArgumentException>()
+                  .And.Message
+                  .EqualTo("Permision id can't be empty."),
+              () => new PermissionId(string.Empty));
+        }
+
+        [TestCase("My permission")]
+        [TestCase("My$permission")]
+        [TestCase("$%^MyPermission!@#")]
+        [TestCase("My permission 1")]
+        [TestCase("My permission 1 !@")]
+        public void TestConstructing_WhenIncorrectNameGiven_ThenArgumentExceptionIsThrown(string name)
+        {
+            Assert.Throws(
+              Is.InstanceOf<ArgumentException>()
+                  .And.Message
+                  .EqualTo("Permision id must contain only alphanumeric characters without spaces."),
+              () => new PermissionId(name));
+        }
+
+        [TestCase("MyPermission")]
+        [TestCase("example1permision")]
+        [TestCase("GetProducts")]
+        [TestCase("asdghxcbnm123456890asdggh")]
+        public void TestConstructing_WhenCorrectNameGiven_ThenNameIsSet(string name)
+        {
+            var permissionId = new PermissionId(name);
+
+            Assert.That(permissionId.ToString(), Is.EqualTo(name));
+        }
+    }
+}
