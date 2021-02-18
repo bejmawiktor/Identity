769bf2cff6d02124d75eb9628abdcdcde6e7cceb
bejmawiktor <bejmawiktor@gmail.com>
2021-02-18 15:46:04 +0100

    Permission added.

---
+++ Identity.Domain/Permission.cs
@@ -0,0 +1,30 @@
+﻿using DDD.Model;
+using System;
+
+namespace Identity.Domain
+{
+    public class Permission : Entity<PermissionId>
+    {
+        public string Description { get; }
+
+        public Permission(PermissionId id, string description) : base(id)
+        {
+            this.ValidateMembers(description);
+
+            this.Description = description;
+        }
+
+        private void ValidateMembers(string description)
+        {
+            if(description == null)
+            {
+                throw new ArgumentNullException(nameof(description));
+            }
+
+            if(description.Length == 0)
+            {
+                throw new ArgumentException("Description can't be empty.");
+            }
+        }
+    }
+}
---
+++ Identity.Tests/Unit/Domain/PermissionTest.cs
@@ -0,0 +1,44 @@
+﻿using Identity.Domain;
+using NUnit.Framework;
+using System;
+
+namespace Identity.Tests.Unit.Domain
+{
+    [TestFixture]
+    public class PermissionTest
+    {
+        [Test]
+        public void TestConstruction_WhenDescriptionGiven_ThenDescriptionIsSet()
+        {
+            var permission = new Permission(
+                id: new PermissionId("GrantPermission"),
+                description: "It allows user to grant permission to other users.");
+
+            Assert.That(permission.Description, Is.EqualTo("It allows user to grant permission to other users."));
+        }
+
+        [Test]
+        public void TestConstructing_WhenEmptyDescriptionGiven_ThenArgumentExceptionIsThrown()
+        {
+            Assert.Throws(
+                Is.InstanceOf<ArgumentException>()
+                    .And.Message
+                    .EqualTo("Description can't be empty."),
+                () => new Permission(
+                    id: new PermissionId("GrantPermission"),
+                    description: string.Empty));
+        }
+
+        [Test]
+        public void TestConstructing_WhenNullDescriptionGiven_ThenArgumentNullExceptionIsThrown()
+        {
+            Assert.Throws(
+                Is.InstanceOf<ArgumentNullException>()
+                    .And.Property(nameof(ArgumentNullException.ParamName))
+                    .EqualTo("description"),
+                () => new Permission(
+                    id: new PermissionId("GrantPermission"),
+                    description: null));
+        }
+    }
+}
