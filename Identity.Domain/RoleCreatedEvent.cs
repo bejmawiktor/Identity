734d0b69a78e305e6ed641ab79f5944c162a09cb
BlekU <bejmawiktor@gmail.com>
2021-02-22 21:24:50 +0100

    RoleCreatedEvent added.

---
+++ Identity.Domain/RoleCreatedEvent.cs
@@ -0,0 +1,22 @@
+﻿using DDD.Events;
+using System;
+
+namespace Identity.Domain
+{
+    public class RoleCreatedEvent : Event
+    {
+        public RoleId RoleId { get; }
+        public string Name { get; }
+        public string Description { get; }
+
+        public RoleCreatedEvent(
+            RoleId roleId,
+            string name,
+            string description)
+        {
+            this.RoleId = roleId;
+            this.Name = name;
+            this.Description = description;
+        }
+    }
+}
---
+++ Identity.Tests/Unit/Domain/RoleCreatedEventTest.cs
@@ -0,0 +1,53 @@
+﻿using Identity.Domain;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Tests.Unit.Domain
+{
+    [TestFixture]
+    public class RoleCreatedEventTest
+    {
+        [Test]
+        public void TestConstruction_WhenRoleIdGiven_ThenRoleIdIsSet()
+        {
+            var roleId = RoleId.Generate();
+
+            var roleCreatedEvent = new RoleCreatedEvent(
+                roleId: roleId,
+                name: "RoleName",
+                description: "Test role description");
+
+            Assert.That(roleCreatedEvent.RoleId, Is.EqualTo(roleId));
+        }
+
+        [Test]
+        public void TestConstruction_WhenNameGiven_ThenNameIsSet()
+        {
+            var roleId = RoleId.Generate();
+
+            var roleCreatedEvent = new RoleCreatedEvent(
+                roleId: roleId,
+                name: "RoleName",
+                description: "Test role description");
+
+            Assert.That(roleCreatedEvent.Name, Is.EqualTo("RoleName"));
+        }
+
+        [Test]
+        public void TestConstruction_WhenDescriptionGiven_ThenDescriptionIsSet()
+        {
+            var roleId = RoleId.Generate();
+
+            var roleCreatedEvent = new RoleCreatedEvent(
+                roleId: roleId,
+                name: "RoleName",
+                description: "Test role description");
+
+            Assert.That(roleCreatedEvent.Description, Is.EqualTo("Test role description"));
+        }
+    }
+}
