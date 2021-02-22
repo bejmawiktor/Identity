b950e87ae96bfd703055163888330eb0fd7ddea7
BlekU <bejmawiktor@gmail.com>
2021-02-22 21:00:52 +0100

    PermissionCreatedEvent added.

---
+++ Identity.Domain/PermissionCreatedEvent.cs
@@ -0,0 +1,19 @@
+﻿using DDD.Events;
+using System;
+
+namespace Identity.Domain
+{
+    public class PermissionCreatedEvent : Event
+    {
+        public PermissionId PermissionId { get; }
+        public string Description { get; }
+
+        internal PermissionCreatedEvent(
+            PermissionId permissionId,
+            string description)
+        {
+            this.PermissionId = permissionId;
+            this.Description = description;
+        }
+    }
+}
---
+++ Identity.Tests/Unit/Domain/PermissionCreatedEventTest.cs
@@ -0,0 +1,35 @@
+﻿using Identity.Domain;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Tests.Unit.Domain
+{
+    [TestFixture]
+    public class PermissionCreatedEventTest
+    {
+        [Test]
+        public void TestConstruction_WhenPermissionIdGiven_ThenPermissionIdIsSet()
+        {
+            var permissionId = new PermissionId(new ResourceId("TestResource"), "MyPermission");
+            var permissionCreatedEvent = new PermissionCreatedEvent(
+                permissionId: permissionId,
+                description: "Test permission description");
+
+            Assert.That(permissionCreatedEvent.PermissionId, Is.EqualTo(permissionId));
+        }
+
+        [Test]
+        public void TestConstruction_WhenDescriptionGiven_ThenDescriptionIsSet()
+        {
+            var permissionCreatedEvent = new PermissionCreatedEvent(
+                permissionId: new PermissionId(new ResourceId("TestResource"), "MyPermission"),
+                description: "Test permission description");
+
+            Assert.That(permissionCreatedEvent.Description, Is.EqualTo("Test permission description"));
+        }
+    }
+}
