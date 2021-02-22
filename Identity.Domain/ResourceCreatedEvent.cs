cd47143d0aadac98fcb4bdd221322eda2eaec07d
BlekU <bejmawiktor@gmail.com>
2021-02-22 21:00:24 +0100

    ResourceCreatedEvent added.

---
+++ Identity.Domain/ResourceCreatedEvent.cs
@@ -0,0 +1,16 @@
+﻿using DDD.Events;
+
+namespace Identity.Domain
+{
+    public class ResourceCreatedEvent : Event
+    {
+        public ResourceId ResourceId { get; }
+        public string Description { get; }
+
+        internal ResourceCreatedEvent(ResourceId resourceId, string description)
+        {
+            this.ResourceId = resourceId;
+            this.Description = description;
+        }
+    }
+}
---
+++ Identity.Tests/Unit/Domain/ResourceCreatedEventTest.cs
@@ -0,0 +1,29 @@
+﻿using Identity.Domain;
+using NUnit.Framework;
+
+namespace Identity.Tests.Unit.Domain
+{
+    [TestFixture]
+    public class ResourceCreatedEventTest
+    {
+        [Test]
+        public void TestConstruction_WhenResourceIdGiven_ThenResourceIdIsSet()
+        {
+            var resourceCreatedEvent = new ResourceCreatedEvent(
+                resourceId: new ResourceId("TestResource"),
+                description: "Test resource description");
+
+            Assert.That(resourceCreatedEvent.ResourceId, Is.EqualTo(new ResourceId("TestResource")));
+        }
+
+        [Test]
+        public void TestConstruction_WhenDescriptionGiven_ThenDescriptionIsSet()
+        {
+            var resourceCreatedEvent = new ResourceCreatedEvent(
+                resourceId: new ResourceId("TestResource"),
+                description: "Test resource description");
+
+            Assert.That(resourceCreatedEvent.Description, Is.EqualTo("Test resource description"));
+        }
+    }
+}
