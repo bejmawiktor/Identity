2ca34b5518024a4ae8b067d3d7d8c2550f38a959
bejmawiktor <bejmawiktor@gmail.com>
2021-02-23 11:58:30 +0100

    RolePermissionRevokedEvent added.

---
+++ Identity.Domain/RolePermissionRevokedEvent.cs
@@ -0,0 +1,18 @@
+﻿using DDD.Events;
+
+namespace Identity.Domain
+{
+    public class RolePermissionRevokedEvent : Event
+    {
+        public RoleId RoleId { get; }
+        public PermissionId RevokedPermissionId { get; }
+
+        internal RolePermissionRevokedEvent(
+            RoleId roleId,
+            PermissionId revokedPermissionId)
+        {
+            this.RoleId = roleId;
+            this.RevokedPermissionId = revokedPermissionId;
+        }
+    }
+}
---
+++ Identity.Tests/Unit/Domain/RolePermissionRevokedEventTest.cs
@@ -0,0 +1,35 @@
+﻿using Identity.Domain;
+using NUnit.Framework;
+
+namespace Identity.Tests.Unit.Domain
+{
+    [TestFixture]
+    public class RolePermissionRevokedEventTest
+    {
+        [Test]
+        public void TestConstruction_WhenRoleIdGiven_ThenRoleIdIsSet()
+        {
+            var roleId = RoleId.Generate();
+            var revokedPermissionId = new PermissionId(new ResourceId("MyResource"), "Permission");
+
+            var rolePermissionRevokedEvent = new RolePermissionRevokedEvent(
+                roleId: roleId,
+                revokedPermissionId: revokedPermissionId);
+
+            Assert.That(rolePermissionRevokedEvent.RoleId, Is.EqualTo(roleId));
+        }
+
+        [Test]
+        public void TestConstruction_WhenRevokedPermissionIdGiven_ThenRevokedPermissionIdIsSet()
+        {
+            var roleId = RoleId.Generate();
+            var revokedPermissionId = new PermissionId(new ResourceId("MyResource"), "Permission");
+
+            var rolePermissionRevokedEvent = new RolePermissionRevokedEvent(
+                roleId: roleId,
+                revokedPermissionId: revokedPermissionId);
+
+            Assert.That(rolePermissionRevokedEvent.RevokedPermissionId, Is.EqualTo(revokedPermissionId));
+        }
+    }
+}
