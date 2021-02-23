b185ce984728dc488d973b9f7cd21957fe750448
bejmawiktor <bejmawiktor@gmail.com>
2021-02-23 11:57:48 +0100

    RolePermissionObtainedEvent added.

---
+++ Identity.Domain/RolePermissionObtainedEvent.cs
@@ -0,0 +1,18 @@
+﻿using DDD.Events;
+
+namespace Identity.Domain
+{
+    public class RolePermissionObtainedEvent : Event
+    {
+        public RoleId RoleId { get; }
+        public PermissionId ObtainedPermissionId { get; }
+
+        internal RolePermissionObtainedEvent(
+            RoleId roleId,
+            PermissionId obtainedPermissionId)
+        {
+            this.RoleId = roleId;
+            this.ObtainedPermissionId = obtainedPermissionId;
+        }
+    }
+}
---
+++ Identity.Tests/Unit/Domain/RolePermissionObtainedEventTest.cs
@@ -0,0 +1,35 @@
+﻿using Identity.Domain;
+using NUnit.Framework;
+
+namespace Identity.Tests.Unit.Domain
+{
+    [TestFixture]
+    public class RolePermissionObtainedEventTest
+    {
+        [Test]
+        public void TestConstruction_WhenRoleIdGiven_ThenRoleIdIsSet()
+        {
+            var roleId = RoleId.Generate();
+            var obtainedPermissionId = new PermissionId(new ResourceId("MyResource"), "Permission");
+
+            var roleCreatedEvent = new RolePermissionObtainedEvent(
+                roleId: roleId,
+                obtainedPermissionId: obtainedPermissionId);
+
+            Assert.That(roleCreatedEvent.RoleId, Is.EqualTo(roleId));
+        }
+
+        [Test]
+        public void TestConstruction_WhenObtainedPermissionIdGiven_ThenObtainedPermissionIdIsSet()
+        {
+            var roleId = RoleId.Generate();
+            var obtainedPermissionId = new PermissionId(new ResourceId("MyResource"), "Permission");
+
+            var roleCreatedEvent = new RolePermissionObtainedEvent(
+                roleId: roleId,
+                obtainedPermissionId: obtainedPermissionId);
+
+            Assert.That(roleCreatedEvent.ObtainedPermissionId, Is.EqualTo(obtainedPermissionId));
+        }
+    }
+}
