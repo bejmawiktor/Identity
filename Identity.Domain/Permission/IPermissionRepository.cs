5c5fbf2bd0a611b778986c884c1d9e53e0f0b32b
bejmawiktor <bejmawiktor@gmail.com>
2021-02-24 13:46:33 +0100

    IPermissionRepository added.

---
+++ Identity.Domain/Permission/IPermissionRepository.cs
@@ -0,0 +1,9 @@
+﻿using DDD.Persistence;
+
+namespace Identity.Domain
+{
+    public interface IPermissionRepository 
+    : IRepository<Permission, PermissionId>, IAsyncRepository<Permission, PermissionId>
+    {
+    }
+}
