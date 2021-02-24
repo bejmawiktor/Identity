1ee377f88b7a47bf151e0464d442c4d01b533163
bejmawiktor <bejmawiktor@gmail.com>
2021-02-24 11:56:21 +0100

    IResourceRepository added.

---
+++ Identity.Domain/Resource/IResourceRepository.cs
@@ -0,0 +1,9 @@
+﻿using DDD.Persistence;
+
+namespace Identity.Domain
+{
+    public interface IResourceRepository
+    : IRepository<Resource, ResourceId>, IAsyncRepository<Resource, ResourceId>
+    {
+    }
+}
