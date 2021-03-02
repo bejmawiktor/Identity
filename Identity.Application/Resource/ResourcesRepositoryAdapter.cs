6d9e32b07915ec3a9776b7fa485e367455b8a1c3
bejmawiktor <bejmawiktor@gmail.com>
2021-03-02 12:15:48 +0100

    ResourcesRepositoryAdapter added.

---
+++ Identity.Application/Resource/ResourcesRepositoryAdapter.cs
@@ -0,0 +1,29 @@
+﻿using DDD.Application.Persistence.Adapters;
+using Identity.Domain;
+using System;
+
+namespace Identity.Application
+{
+    using IAsyncResourcesRepositoryAdapter = IAsyncRepositoryAdapter<ResourceDto, string, IResourcesRepository, ResourceDtoConverter, Resource, ResourceId>;
+    using IResourcesRepositoryAdapter = IRepositoryAdapter<ResourceDto, string, IResourcesRepository, ResourceDtoConverter, Resource, ResourceId>;
+
+    internal class ResourcesRepositoryAdapter
+    : IAsyncResourcesRepositoryAdapter,
+        IResourcesRepositoryAdapter,
+        Identity.Domain.IResourcesRepository
+    {
+        public IResourcesRepository ResourcesRepository { get; }
+
+        IResourcesRepository IAsyncResourcesRepositoryAdapter.DtoRepository
+            => this.ResourcesRepository;
+
+        IResourcesRepository IResourcesRepositoryAdapter.DtoRepository
+            => this.ResourcesRepository;
+
+        public ResourcesRepositoryAdapter(IResourcesRepository resourcesRepository)
+        {
+            this.ResourcesRepository = resourcesRepository
+                ?? throw new ArgumentNullException(nameof(resourcesRepository));
+        }
+    }
+}
---
+++ Identity.Tests/Unit/Application/Resource/ResourcesRepositoryAdapterTest.cs
@@ -0,0 +1,38 @@
+﻿using DDD.Application.Persistence;
+using DDD.Application.Persistence.Adapters;
+using DDD.Domain.Persistence;
+using Identity.Application;
+using Identity.Domain;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using IResourcesRepository = Identity.Application.IResourcesRepository;
+
+namespace Identity.Tests.Unit.Application
+{
+    [TestFixture]
+    public class ResourcesRepositoryAdapterTest
+    {
+        [Test]
+        public void TestConstructing_WhenNullResourcesRepositoryGiven_ThenArgumentNullExceptionIsThrown()
+        {
+            Assert.Throws(
+               Is.InstanceOf<ArgumentNullException>()
+                   .And.Property(nameof(ArgumentNullException.ParamName))
+                   .EqualTo("resourcesRepository"),
+               () => new ResourcesRepositoryAdapter(null));
+        }
+
+        [Test]
+        public void TestConstructing_WhenResourcesRepositoryGiven_ThenResourcesRepositoryIsSet()
+        {
+            var resourcesRepositoryMock = new Mock<IResourcesRepository>();
+            IResourcesRepository resourcesRepository = resourcesRepositoryMock.Object;
+            var resourcesRepositoryAdapter = new ResourcesRepositoryAdapter(resourcesRepository);
+
+            Assert.That(resourcesRepositoryAdapter.ResourcesRepository, Is.EqualTo(resourcesRepository));
+        }
+    }
+}
