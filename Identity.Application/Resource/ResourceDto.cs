34ff90f591e02623cc3833dc383b5533c98537aa
bejmawiktor <bejmawiktor@gmail.com>
2021-03-02 12:06:54 +0100

    ResourceDto added.

---
+++ Identity.Application/Resource/ResourceDto.cs
@@ -0,0 +1,29 @@
+﻿using DDD.Application.Model;
+using Identity.Domain;
+
+namespace Identity.Application
+{
+    public class ResourceDto : AggregateRootDto<Resource, ResourceId>
+    {
+        public string Id { get; }
+        public string Description { get; }
+
+        public ResourceDto(string id, string description)
+        {
+            this.Id = id;
+            this.Description = description;
+        }
+
+        private ResourceDto()
+        {
+        }
+
+        internal Resource ToResource()
+            => this.ToDomainObject();
+
+        protected override Resource ToDomainObject()
+            => new Resource(
+                id: new ResourceId(this.Id),
+                description: this.Description);
+    }
+}
---
+++ Identity.Tests/Unit/Application/Resource/ResourcesDtoTest.cs
@@ -0,0 +1,40 @@
+﻿using Identity.Application;
+using Identity.Domain;
+using NUnit.Framework;
+
+namespace Identity.Tests.Unit.Application
+{
+    [TestFixture]
+    public class ResourcesDtoTest
+    {
+        [Test]
+        public void TestConstructing_WhenIdGiven_ThenIdIsSet()
+        {
+            var resourceDto = new ResourceDto("MyResource", "My resource description.");
+
+            Assert.That(resourceDto.Id, Is.EqualTo("MyResource"));
+        }
+
+        [Test]
+        public void TestConstructing_WhenDescriptionGiven_ThenDescriptionIsSet()
+        {
+            var resourceDto = new ResourceDto("MyResource", "My resource description.");
+
+            Assert.That(resourceDto.Description, Is.EqualTo("My resource description."));
+        }
+
+        [Test]
+        public void TestToResource_WhenConvertingToResource_ThenResourceIsReturned()
+        {
+            var resourceDto = new ResourceDto("MyResource", "My resource description.");
+
+            Resource resource = resourceDto.ToResource();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(resource.Id, Is.EqualTo(new ResourceId("MyResource")));
+                Assert.That(resource.Description, Is.EqualTo("My resource description."));
+            });
+        }
+    }
+}
