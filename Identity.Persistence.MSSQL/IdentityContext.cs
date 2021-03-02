5e8e1618ac48e40d479e4aa2e172a718d9b65046
bejmawiktor <bejmawiktor@gmail.com>
2021-03-02 13:18:14 +0100

    IdentityContext added.

---
+++ Identity.Persistence.MSSQL/IdentityContext.cs
@@ -0,0 +1,25 @@
+﻿using Identity.Application;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Identity.Persistence.MSSQL
+{
+    public class IdentityContext : DbContext
+    {
+        internal DbSet<ResourceDto> Resources { get; set; }
+        public string ConnectionString { get; }
+
+        public IdentityContext(string connectionString)
+        {
+            this.ConnectionString = connectionString
+                ?? throw new ArgumentNullException(nameof(connectionString));
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                this.ConnectionString,
+                b => b.MigrationsAssembly("Identity.Persistence.MSSQL"));
+        }
+    }
+}
---
+++ Identity.Tests/Unit/Persistence/MSSQL/IdentityContextTest.cs
@@ -0,0 +1,30 @@
+﻿using Identity.Persistence.MSSQL;
+using NUnit.Framework;
+using System;
+
+namespace Identity.Tests.Unit.Persistence.MSSQL
+{
+    [TestFixture]
+    public class IdentityContextTest
+    {
+        [Test]
+        public void TestConstructing_WhenConnectionStringGiven_ThenConnectionStringIsSet()
+        {
+            var identityContext = new IdentityContext("TestConnectionString");
+
+            Assert.Throws(
+                Is.InstanceOf<ArgumentNullException>()
+                    .And.Property(nameof(ArgumentNullException.ParamName))
+                    .EqualTo("connectionString"),
+                () => new IdentityContext(null));
+        }
+
+        [Test]
+        public void TestConstructing_WhenNullConnectionStringGiven_ThenArgumentNullExceptionIsThrown()
+        {
+            var identityContext = new IdentityContext("TestConnectionString");
+
+            Assert.That(identityContext.ConnectionString, Is.EqualTo("TestConnectionString"));
+        }
+    }
+}
