01190db25631ec5eb2be83a509b3dde5a37e445e
bejmawiktor <bejmawiktor@gmail.com>
2021-03-02 12:18:00 +0100

    CreateResourceCommandHandler added.

---
+++ Identity.Application/Resource/CreateResourceCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using DDD.Application.CQRS;
+using Identity.Domain;
+using System;
+using System.Threading.Tasks;
+
+namespace Identity.Application
+{
+    public class CreateResourceCommandHandler
+    : ICommandHandler<CreateResourceCommand>, IAsyncCommandHandler<CreateResourceCommand>
+    {
+        public IResourcesRepository ResourcesRepository { get; }
+        private ResourceService ResourceService { get; }
+
+        public CreateResourceCommandHandler(IResourcesRepository resourcesRepository)
+        {
+            this.ResourcesRepository = resourcesRepository
+                ?? throw new ArgumentNullException(nameof(resourcesRepository));
+            this.ResourceService = new ResourceService(
+                new ResourcesRepositoryAdapter(resourcesRepository));
+        }
+
+        public void Handle(CreateResourceCommand command)
+        {
+            this.ResourceService.CreateResource(command.ResourceId, command.ResourceDescription);
+        }
+
+        public Task HandleAsync(CreateResourceCommand command)
+        {
+            return this.ResourceService.CreateResourceAsync(command.ResourceId, command.ResourceDescription);
+        }
+    }
+}
---
+++ Identity.Tests/Unit/Application/Resource/CreateResourceCommandHandlerTest.cs
@@ -0,0 +1,31 @@
+﻿using Identity.Application;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace Identity.Tests.Unit.Application
+{
+    [TestFixture]
+    public class CreateResourceCommandHandlerTest
+    {
+        [Test]
+        public void TestConstructor_WhenResourcesRepositoryGiven_ThenResourcesRepositoryIsSet()
+        {
+            var resourcesRepositoryMock = new Mock<IResourcesRepository>();
+            IResourcesRepository resourcesRepository = resourcesRepositoryMock.Object;
+            var createResourceCommandHandler = new CreateResourceCommandHandler(resourcesRepository);
+
+            Assert.That(createResourceCommandHandler.ResourcesRepository, Is.EqualTo(resourcesRepository));
+        }
+
+        [Test]
+        public void TestConstructor_WhenNullResourcesRepositoryGiven_ThenArgumentNullExceptionIsThrown()
+        {
+            Assert.Throws(
+               Is.InstanceOf<ArgumentNullException>()
+                   .And.Property(nameof(ArgumentNullException.ParamName))
+                   .EqualTo("resourcesRepository"),
+               () => new CreateResourceCommandHandler(null));
+        }
+    }
+}
