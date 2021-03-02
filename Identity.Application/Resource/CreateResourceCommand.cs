e4011bf7d5236e7b033ad4d7fe3e3f3238c10319
bejmawiktor <bejmawiktor@gmail.com>
2021-03-02 12:16:34 +0100

    CreateResourceCommand added.

---
+++ Identity.Application/Resource/CreateResourceCommand.cs
@@ -0,0 +1,16 @@
+﻿using DDD.Application.CQRS;
+
+namespace Identity.Application
+{
+    public class CreateResourceCommand : ICommand
+    {
+        public string ResourceId { get; }
+        public string ResourceDescription { get; }
+
+        public CreateResourceCommand(string resourceId, string resourceDescription)
+        {
+            this.ResourceId = resourceId;
+            this.ResourceDescription = resourceDescription;
+        }
+    }
+}
---
+++ Identity.Tests/Unit/Application/Resource/CreateResourceCommandTest.cs
@@ -0,0 +1,29 @@
+﻿using Identity.Application;
+using NUnit.Framework;
+
+namespace Identity.Tests.Unit.Application
+{
+    [TestFixture]
+    public class CreateResourceCommandTest
+    {
+        [Test]
+        public void TestConstructing_WhenResourceIdGiven_ThenResourceIdIsSet()
+        {
+            var createResourceCommand = new CreateResourceCommand(
+                resourceId: "MyResource",
+                resourceDescription: "Resource description.");
+
+            Assert.That(createResourceCommand.ResourceId, Is.EqualTo("MyResource"));
+        }
+
+        [Test]
+        public void TestConstructing_WhenResourceDescriptionGiven_ThenResourceDescriptionIsSet()
+        {
+            var createResourceCommand = new CreateResourceCommand(
+                resourceId: "MyResource",
+                resourceDescription: "Resource description.");
+
+            Assert.That(createResourceCommand.ResourceDescription, Is.EqualTo("Resource description."));
+        }
+    }
+}
