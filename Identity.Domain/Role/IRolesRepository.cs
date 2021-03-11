33457a4abccc2e819df202612ceaf9b4ff7a1a9c
bejmawiktor <bejmawiktor@gmail.com>
2021-03-11 15:41:30 +0100

    IRolesRepository added.

---
+++ Identity.Domain/Role/IRolesRepository.cs
@@ -0,0 +1,9 @@
+﻿using DDD.Domain.Persistence;
+
+namespace Identity.Domain
+{
+    public interface IRolesRepository
+    : IRepository<Role, RoleId>, IAsyncRepository<Role, RoleId>
+    {
+    }
+}
