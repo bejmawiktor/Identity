dcf1a3996a3585dd4a1f4b89318686cba24027e9
bejmawiktor <bejmawiktor@gmail.com>
2021-03-11 15:41:47 +0100

    IUsersRepository added.

---
+++ Identity.Domain/User/IUsersRepository.cs
@@ -0,0 +1,9 @@
+﻿using DDD.Domain.Persistence;
+
+namespace Identity.Domain
+{
+    public interface IUsersRepository
+    : IRepository<User, UserId>, IAsyncRepository<User, UserId>
+    {
+    }
+}
