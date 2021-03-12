a414ce9ff57ef92ab996a862415a846cfdd1e80e
bejmawiktor <bejmawiktor@gmail.com>
2021-03-12 13:09:08 +0100

    UserNotFoundException added.

---
+++ Identity.Domain/User/UserNotFoundException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Identity.Domain
+{
+    public class UserNotFoundException : Exception
+    {
+
+        public UserNotFoundException() : base()
+        {
+        }
+
+        public UserNotFoundException(UserId userId)
+        : base($"User {userId} not found.")
+        {
+        }
+
+        public UserNotFoundException(string message) : base(message)
+        {
+        }
+
+        public UserNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
