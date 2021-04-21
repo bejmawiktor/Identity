df963887c5ce3288b99e9a37db600559530fbf57
bejmawiktor <bejmawiktor@gmail.com>
2021-04-21 13:36:16 +0200

    Domain IApplicationsRepository added.

---
+++ Identity.Domain/Application/IApplicationsRepository.cs
@@ -0,0 +1,9 @@
+﻿using DDD.Domain.Persistence;
+
+namespace Identity.Domain
+{
+    public interface IApplicationsRepository 
+    : IRepository<Application, ApplicationId>, IAsyncRepository<Application, ApplicationId>
+    {
+    }
+}
