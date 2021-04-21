cf70766007db80190575d58a1581ac50ffba242a
bejmawiktor <bejmawiktor@gmail.com>
2021-04-21 13:21:05 +0200

    Url added.

---
+++ Identity.Tests/Unit/Domain/Application/UrlTest.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Identity.Tests.Unit.Domain
+{
+    using Url = Identity.Domain.Url;
+
+    [TestFixture]
+    public class UrlTest
+    {
+        [Test]
+        public void TestConstructing_WhenNullValueGiven_ThenArgumentNullExceptionIsThrown()
+        {
+            Assert.Throws(
+                Is.InstanceOf<ArgumentNullException>()
+                    .And.Property(nameof(ArgumentNullException.ParamName))
+                    .EqualTo("value"),
+                () => new Url(null));
+        }
+
+        [Test]
+        public void TestConstructing_WhenEmptyValueGiven_ThenArgumentExceptionIsThrown()
+        {
+            Assert.Throws(
+                Is.InstanceOf<ArgumentException>()
+                    .And.Message
+                    .EqualTo("Url can't be empty."),
+                () => new Url(string.Empty));
+        }
+
+        [TestCase("http://.www.foo.bar./")]
+        [TestCase("ftps://foo.bar/")]
+        [TestCase("ftp://foo.bar/")]
+        [TestCase(":// should fail")]
+        [TestCase("http:// shouldfail.com")]
+        [TestCase("//")]
+        [TestCase("http://##/")]
+        [TestCase("asfasfsgaf gafg ")]
+        [TestCase(" ")]
+        [TestCase("www.foo.bar")]
+        public void TestConstructing_WhenInvalidUrlGiven_ThenArgumentExceptionIsThrown(string value)
+        {
+            Assert.Throws(
+                Is.InstanceOf<ArgumentException>()
+                    .And.Message
+                    .EqualTo("Invalid url given."),
+                () => new Url(value));
+        }
+
+        [TestCase("http://www.foo.bar/")]
+        [TestCase("http://foo.com/blah_blah/")]
+        [TestCase("https://www.example.com/foo/?bar=baz&inga=42&quux")]
+        [TestCase("https://www.google.pl")]
+        public void TestToString_WhenValidUrlGiven_ThenUrlValueIsReturned(string value)
+        {
+            var url = new Url(value);
+
+            Assert.That(url.ToString(), Is.EqualTo(value));
+        }
+    }
+}
