060a5770db3c1ed09bdd590ae27855697aff7cad
bejmawiktor <bejmawiktor@gmail.com>
2021-10-19 10:16:19 +0200

    IAuthorizationCodesRepository added.

---
+++ Identity.Domain/AuthorizationCode/IAuthorizationCodesRepository.cs
@@ -0,0 +1,9 @@
+﻿using DDD.Domain.Persistence;
+
+namespace Identity.Domain
+{
+    public interface IAuthorizationCodesRepository
+    : IRepository<AuthorizationCode, AuthorizationCodeId>, IAsyncRepository<AuthorizationCode, AuthorizationCodeId>
+    {
+    }
+}
