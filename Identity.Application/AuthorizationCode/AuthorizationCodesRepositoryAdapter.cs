52850af0aeea3c7becdcd31cae4c05790d705b98
bejmawiktor <bejmawiktor@gmail.com>
2021-10-19 10:23:26 +0200

    AuthorizationCodesRepositoryAdapter added.

---
+++ Identity.Application/AuthorizationCode/AuthorizationCodesRepositoryAdapter.cs
@@ -0,0 +1,26 @@
+﻿using DDD.Application.Persistence.Adapters;
+using System;
+
+namespace Identity.Application
+{
+    using IAsyncAuthorizationCodesRepositoryAdapter = IAsyncRepositoryAdapter<AuthorizationCodeDto, (Guid ApplicationId, string Code), IAuthorizationCodesRepository, AuthorizationCodeDtoConverter, Domain.AuthorizationCode, Domain.AuthorizationCodeId>;
+    using IAuthorizationCodesRepositoryAdapter = IRepositoryAdapter<AuthorizationCodeDto, (Guid ApplicationId, string Code), IAuthorizationCodesRepository, AuthorizationCodeDtoConverter, Domain.AuthorizationCode, Domain.AuthorizationCodeId>;
+
+    internal class AuthorizationCodesRepositoryAdapter 
+    : IAuthorizationCodesRepositoryAdapter, IAsyncAuthorizationCodesRepositoryAdapter, Domain.IAuthorizationCodesRepository
+    {
+        public IAuthorizationCodesRepository AuthorizationCodesRepository { get; }
+
+        IAuthorizationCodesRepository IAsyncAuthorizationCodesRepositoryAdapter.DtoRepository
+            => this.AuthorizationCodesRepository;
+
+        IAuthorizationCodesRepository IAuthorizationCodesRepositoryAdapter.DtoRepository
+            => this.AuthorizationCodesRepository;
+
+        public AuthorizationCodesRepositoryAdapter(IAuthorizationCodesRepository authorizationCodesRepository)
+        {
+            this.AuthorizationCodesRepository = authorizationCodesRepository
+                ?? throw new ArgumentNullException(nameof(authorizationCodesRepository));
+        }
+    }
+}
