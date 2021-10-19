87d4621f3cf73f5af5ef964876e484611d40294e
bejmawiktor <bejmawiktor@gmail.com>
2021-10-19 10:17:51 +0200

    Dto IAuthorizationCodesRepository added.

---
+++ Identity.Application/AuthorizationCode/IAuthorizationCodesRepository.cs
@@ -0,0 +1,10 @@
+﻿using DDD.Application.Persistence;
+using System;
+
+namespace Identity.Application
+{
+    public interface IAuthorizationCodesRepository : IAsyncDtoRepository<AuthorizationCodeDto, (Guid ApplicationId, string Code)>,
+        IDtoRepository<AuthorizationCodeDto, (Guid ApplicationId, string Code)>
+    {
+    }
+}
