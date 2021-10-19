34fb3da56732ea66cc4a1314400036b0fd1cf517
bejmawiktor <bejmawiktor@gmail.com>
2021-10-19 09:38:33 +0200

    AuthorizationCodeDto added.

---
+++ Identity.Application/AuthorizationCode/AuthorizationCodeDto.cs
@@ -0,0 +1,51 @@
+﻿using DDD.Application.Model;
+using Identity.Domain;
+using System;
+
+namespace Identity.Application
+{
+    using ApplicationId = Domain.ApplicationId;
+
+    public class AuthorizationCodeDto : IAggregateRootDto<AuthorizationCode, AuthorizationCodeId>
+    {
+        public string Code { get; set; }
+        public Guid ApplicationId { get; set; }
+        public DateTime ExpiresAt { get; set; }
+        public bool Used { get; set; }
+
+        public AuthorizationCodeDto(
+            string code,
+            Guid applicationId,
+            DateTime expiresAt,
+            bool used)
+        {
+            this.Code = code;
+            this.ApplicationId = applicationId;
+            this.ExpiresAt = expiresAt;
+            this.Used = used;
+        }
+
+        AuthorizationCode IDomainObjectDto<AuthorizationCode>.ToDomainObject()
+            => this.ToAuthorizationCode();
+
+        public AuthorizationCode ToAuthorizationCode()
+            => new AuthorizationCode(
+                new AuthorizationCodeId(this.Code, new ApplicationId(this.ApplicationId)),
+                this.ExpiresAt,
+                this.Used);
+
+        public override bool Equals(object obj)
+        {
+            return obj is AuthorizationCodeDto dto
+                && this.Code == dto.Code
+                && this.ApplicationId.Equals(dto.ApplicationId)
+                && this.ExpiresAt == dto.ExpiresAt
+                && this.Used == dto.Used;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this.Code, this.ApplicationId, this.ExpiresAt, this.Used);
+        }
+    }
+}
---
+++ Identity.Tests/Unit/Application/AuthorizationCode/AuthorizationCodeDtoTest.cs
@@ -0,0 +1,174 @@
+﻿using Identity.Application;
+using Identity.Domain;
+using NUnit.Framework;
+using System;
+
+namespace Identity.Tests.Unit.Application
+{
+    using ApplicationId = Identity.Domain.ApplicationId;
+
+    [TestFixture]
+    public class AuthorizationCodeDtoTest
+    {
+        [Test]
+        public void TestConstructing_WhenCodeGiven_ThenCodeIsSet()
+        {
+            ApplicationId applicationId = ApplicationId.Generate();
+            AuthorizationCodeId authorizationCodeId = AuthorizationCodeId.Generate(applicationId);
+            var authorizationCodeDto = new AuthorizationCodeDto(
+                code: authorizationCodeId.Code,
+                applicationId: applicationId.ToGuid(),
+                expiresAt: DateTime.Now,
+                used: true);
+
+            Assert.That(authorizationCodeDto.Code, Is.EqualTo(authorizationCodeId.Code));
+        }
+
+        [Test]
+        public void TestConstructing_WhenApplicationIdGiven_ThenApplicationIdIsSet()
+        {
+            ApplicationId applicationId = ApplicationId.Generate();
+            AuthorizationCodeId authorizationCodeId = AuthorizationCodeId.Generate(applicationId);
+            var authorizationCodeDto = new AuthorizationCodeDto(
+                code: authorizationCodeId.Code,
+                applicationId: applicationId.ToGuid(),
+                expiresAt: DateTime.Now,
+                used: true);
+
+            Assert.That(authorizationCodeDto.ApplicationId, Is.EqualTo(applicationId.ToGuid()));
+        }
+
+        [Test]
+        public void TestConstructing_WhenExpiresAtGiven_ThenExpiresAtIsSet()
+        {
+            DateTime expiresAt = DateTime.Now;
+            ApplicationId applicationId = ApplicationId.Generate();
+            AuthorizationCodeId authorizationCodeId = AuthorizationCodeId.Generate(applicationId);
+            var authorizationCodeDto = new AuthorizationCodeDto(
+                code: authorizationCodeId.Code,
+                applicationId: applicationId.ToGuid(),
+                expiresAt: expiresAt,
+                used: true);
+
+            Assert.That(authorizationCodeDto.ExpiresAt, Is.EqualTo(expiresAt));
+        }
+
+        [Test]
+        public void TestConstructing_WhenUsedGiven_ThenUsedIsSet()
+        {
+            DateTime expiresAt = DateTime.Now;
+            ApplicationId applicationId = ApplicationId.Generate();
+            AuthorizationCodeId authorizationCodeId = AuthorizationCodeId.Generate(applicationId);
+            var authorizationCodeDto = new AuthorizationCodeDto(
+                code: authorizationCodeId.Code,
+                applicationId: applicationId.ToGuid(),
+                expiresAt: expiresAt,
+                used: true);
+
+            Assert.That(authorizationCodeDto.Used, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void TestEquals_WhenTwoIdenticalAuthorizationCodesDtosGiven_ThenTrueIsReturned()
+        {
+            DateTime expiresAt = DateTime.Now;
+            ApplicationId applicationId = ApplicationId.Generate();
+            AuthorizationCodeId authorizationCodeId = AuthorizationCodeId.Generate(applicationId);
+            var firstAuthorizationCodeDto = new AuthorizationCodeDto(
+                code: authorizationCodeId.Code,
+                applicationId: applicationId.ToGuid(),
+                expiresAt: expiresAt,
+                used: true);
+            var secondAuthorizationCodeDto = new AuthorizationCodeDto(
+                code: authorizationCodeId.Code,
+                applicationId: applicationId.ToGuid(),
+                expiresAt: expiresAt,
+                used: true);
+
+            Assert.That(firstAuthorizationCodeDto.Equals(secondAuthorizationCodeDto), Is.True);
+        }
+
+        [Test]
+        public void TestEquals_WhenTwoDifferentAuthorizationCodesDtosGiven_ThenTrueIsReturned()
+        {
+            DateTime expiresAt = DateTime.Now;
+            ApplicationId applicationId = ApplicationId.Generate();
+            AuthorizationCodeId authorizationCodeId = AuthorizationCodeId.Generate(applicationId);
+            var firstAuthorizationCodeDto = new AuthorizationCodeDto(
+                code: authorizationCodeId.Code,
+                applicationId: applicationId.ToGuid(),
+                expiresAt: expiresAt,
+                used: true);
+            var secondAuthorizationCodeDto = new AuthorizationCodeDto(
+                code: authorizationCodeId.Code,
+                applicationId: applicationId.ToGuid(),
+                expiresAt: expiresAt,
+                used: false);
+
+            Assert.That(firstAuthorizationCodeDto.Equals(secondAuthorizationCodeDto), Is.False);
+        }
+
+        [Test]
+        public void TestGetHashCode_WhenTwoIdenticalAuthorizationCodesDtosGiven_ThenHashCodesAreEqual()
+        {
+            DateTime expiresAt = DateTime.Now;
+            ApplicationId applicationId = ApplicationId.Generate();
+            AuthorizationCodeId authorizationCodeId = AuthorizationCodeId.Generate(applicationId);
+            var firstAuthorizationCodeDto = new AuthorizationCodeDto(
+                code: authorizationCodeId.Code,
+                applicationId: applicationId.ToGuid(),
+                expiresAt: expiresAt,
+                used: true);
+            var secondAuthorizationCodeDto = new AuthorizationCodeDto(
+                code: authorizationCodeId.Code,
+                applicationId: applicationId.ToGuid(),
+                expiresAt: expiresAt,
+                used: true);
+
+            Assert.That(firstAuthorizationCodeDto.GetHashCode(), Is.EqualTo(secondAuthorizationCodeDto.GetHashCode()));
+        }
+
+        [Test]
+        public void TestGetHashCode_WhenTwoDifferentAuthorizationCodesDtosGiven_ThenHashCodesAreNotEqual()
+        {
+            DateTime expiresAt = DateTime.Now;
+            ApplicationId applicationId = ApplicationId.Generate();
+            AuthorizationCodeId authorizationCodeId = AuthorizationCodeId.Generate(applicationId);
+            var firstAuthorizationCodeDto = new AuthorizationCodeDto(
+                code: authorizationCodeId.Code,
+                applicationId: applicationId.ToGuid(),
+                expiresAt: expiresAt,
+                used: true);
+            var secondAuthorizationCodeDto = new AuthorizationCodeDto(
+                code: authorizationCodeId.Code,
+                applicationId: applicationId.ToGuid(),
+                expiresAt: expiresAt,
+                used: false);
+
+            Assert.That(firstAuthorizationCodeDto.GetHashCode(), Is.Not.EqualTo(secondAuthorizationCodeDto.GetHashCode()));
+        }
+
+        [Test]
+        public void TestToAuthorizationCode_WhenConvertingToAuthorizationCode_ThenAuthorizationCodeIsReturned()
+        {
+            DateTime expiresAt = DateTime.Now;
+            ApplicationId applicationId = ApplicationId.Generate();
+            AuthorizationCodeId authorizationCodeId = AuthorizationCodeId.Generate(applicationId);
+            var authorizationCodeDto = new AuthorizationCodeDto(
+                code: authorizationCodeId.Code,
+                applicationId: authorizationCodeId.ApplicationId.ToGuid(),
+                expiresAt: expiresAt,
+                used: true);
+
+            AuthorizationCode authorizationCode = authorizationCodeDto.ToAuthorizationCode();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(authorizationCode.Id.Code, Is.EqualTo(authorizationCodeId.Code));
+                Assert.That(authorizationCode.Id.ApplicationId, Is.EqualTo(applicationId));
+                Assert.That(authorizationCode.ExpiresAt, Is.EqualTo(expiresAt));
+                Assert.That(authorizationCode.Used, Is.True);
+            });
+        }
+    }
+}
