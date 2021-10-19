4ce6fa42da9ed087d403a8c8c7a51c21e8bf1b37
bejmawiktor <bejmawiktor@gmail.com>
2021-10-19 09:41:21 +0200

    AuthorizationCodeDtoConverter added.

---
+++ Identity.Application/AuthorizationCode/AuthorizationCodeDtoConverter.cs
@@ -0,0 +1,34 @@
+﻿using DDD.Application.Model.Converters;
+using Identity.Domain;
+using System;
+
+namespace Identity.Application
+{
+    public class AuthorizationCodeDtoConverter
+    : IAggregateRootDtoConverter<AuthorizationCode, AuthorizationCodeId, AuthorizationCodeDto, (Guid ApplicationId, string Code)>
+    {
+        public AuthorizationCodeDto ToDto(AuthorizationCode authorizationCode)
+        {
+            if (authorizationCode == null)
+            {
+                throw new ArgumentNullException(nameof(authorizationCode));
+            }
+
+            return new AuthorizationCodeDto(
+                code: authorizationCode.Id.Code,
+                applicationId: authorizationCode.Id.ApplicationId.ToGuid(),
+                expiresAt: authorizationCode.ExpiresAt,
+                used: authorizationCode.Used);
+        }
+
+        public (Guid ApplicationId, string Code) ToDtoIdentifier(AuthorizationCodeId authorizationCodeId)
+        {
+            if(authorizationCodeId == null)
+            {
+                throw new ArgumentNullException(nameof(authorizationCodeId));
+            }
+
+            return (authorizationCodeId.ApplicationId.ToGuid(), authorizationCodeId.Code);
+        }
+    }
+}
---
+++ Identity.Tests/Unit/Application/AuthorizationCode/AuthorizationCodeDtoConverterTest.cs
@@ -0,0 +1,72 @@
+﻿using Identity.Application;
+using Identity.Domain;
+using NUnit.Framework;
+using System;
+
+namespace Identity.Tests.Unit.Application
+{
+    using ApplicationId = Identity.Domain.ApplicationId;
+
+    [TestFixture]
+    public class AuthorizationCodeDtoConverterTest
+    {
+        [Test]
+        public void TestToDto_WhenAuthorizationCodeGiven_ThenAuthorizationCodeDtoIsReturned()
+        {
+            DateTime expiresAt = DateTime.Now;
+            ApplicationId applicationId = ApplicationId.Generate();
+            AuthorizationCodeId authorizationCodeId = AuthorizationCodeId.Generate(applicationId);
+            var authorizationCode = new AuthorizationCode(
+                id: authorizationCodeId,
+                expiresAt: expiresAt,
+                used: true);
+            var authorizationCodeDtoConverter = new AuthorizationCodeDtoConverter();
+
+            var authorizationCodeDto = authorizationCodeDtoConverter.ToDto(authorizationCode);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(authorizationCodeDto.ApplicationId, Is.EqualTo(authorizationCodeId.ApplicationId.ToGuid()));
+                Assert.That(authorizationCodeDto.Code, Is.EqualTo(authorizationCodeId.Code));
+                Assert.That(authorizationCodeDto.ExpiresAt, Is.EqualTo(expiresAt));
+                Assert.That(authorizationCodeDto.Used, Is.True);
+            });
+        }
+
+        [Test]
+        public void TestToDto_WhenNullAuthorizationCodeGiven_ThenArgumentNullExceptionIsThrown()
+        {
+            var authorizationCodeDtoConverter = new AuthorizationCodeDtoConverter();
+
+            Assert.Throws(
+                Is.InstanceOf<ArgumentNullException>()
+                    .And.Property(nameof(ArgumentNullException.ParamName))
+                    .EqualTo("authorizationCode"),
+                () => authorizationCodeDtoConverter.ToDto(null));
+        }
+
+        [Test]
+        public void TestToDtoIdentifier_WhenAuthorizationCodeIdGiven_ThenDtoIdentifierIsReturned()
+        {
+            ApplicationId applicationId = ApplicationId.Generate();
+            AuthorizationCodeId authorizationCodeId = AuthorizationCodeId.Generate(applicationId);
+            var authorizationCodeDtoConverter = new AuthorizationCodeDtoConverter();
+
+            (Guid ApplicationId, string Code) authorizationCodeDtoId = authorizationCodeDtoConverter.ToDtoIdentifier(authorizationCodeId);
+
+            Assert.That(authorizationCodeDtoId, Is.EqualTo((authorizationCodeId.ApplicationId.ToGuid(), authorizationCodeId.Code)));
+        }
+
+        [Test]
+        public void TestToDtoIdentifier_WhenNullAuthorizationCodeIdGiven_ThenArgumentNullExceptionIsThrown()
+        {
+            var authorizationCodeDtoConverter = new AuthorizationCodeDtoConverter();
+
+            Assert.Throws(
+                Is.InstanceOf<ArgumentNullException>()
+                    .And.Property(nameof(ArgumentNullException.ParamName))
+                    .EqualTo("authorizationCodeId"),
+                () => authorizationCodeDtoConverter.ToDtoIdentifier(null));
+        }
+    }
+}
